112d36c0f0453d25656a7de00f39cfa5d42e971f
Jonathan Hankey <lanfeust23@gmail.com>
2018-01-18 22:02:51 -0800

    Fixed casing for cryptocompare api call

--- src/repositories/CryptoPricesRepository.js
+++ src/repositories/CryptoPricesRepository.js
@@ -4,7 +4,7 @@ export default class CryptoPricesRepository {
     constructor() { }
 
     getPrices(coins) {
-        var coinsString = coins.join()
+        var coinsString = coins.join().toUpperCase()
 
         return fetchival(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsString}&tsyms=USD`).get();
     }
